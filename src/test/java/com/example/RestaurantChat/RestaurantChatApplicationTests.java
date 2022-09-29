59281cd52c7c96786e1191f433028808a5377963
Manal El Outmani <manal.eloutmani20@gmail.com>
2022-09-29 11:29:32 +0100

    structure of the project

---
+++ src/test/java/com/example/RestaurantChat/RestaurantChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
