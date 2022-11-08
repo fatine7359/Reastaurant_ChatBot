936413e5003520dcf3863a9e2770c47ba94d75da
Fatine <hariri.fatine17@gmail.com>
2022-11-08 09:14:41 -0800

    updating frontend and applying best practices

---
+++ Backend/src/main/java/com/example/RestaurantChat/services/InitializeEnglishAnswers.java
@@ -0,0 +1,38 @@
+package com.example.RestaurantChat.services;
+
+import java.util.Map;
+
+public class InitializeEnglishAnswers {
+
+    public void initializeAnswers(Map<String, String> questionAnswer){
+
+        questionAnswer.put("greeting", "Hello, how can I help you?");
+        questionAnswer.put("menu-inquiry",
+                "for the menu whe have:\n" +
+                        "as appetizers: Guacamole, Balsamic Bruschetta, Hot Spinach Artichoke Dip.\n" +
+                        "as main dishes: Cedar-Plank Salmon, Seared Scallops With Brown Butter and Lemon Pan Sauce.\n" +
+                        "as dessert: Chocolate Raspberry Mousse, BrowniesChocolate, Cheesecake.\n");
+        questionAnswer.put("price-inquiry", "appetizers: 20$ -- 25$\n" +
+                "main dish: 25$ -- 70$\n" +
+                "dessert: 30$ -- 50$\n");
+        questionAnswer.put("conversation-continue", "What else can I help you with?");
+        questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
+
+    }
+
+    public void initializeFrAnswers(Map<String, String> questionAnswerFr){
+
+        questionAnswerFr.put("greeting", "Salut, Comment je peux vous aider?");
+        questionAnswerFr.put("menu-inquiry",
+                "Pour le menu on a:\n" +
+                        "Comme entrée: Guacamole, Velouté potimarron et lait d'amande, Oeufs mollets champignons bacon.\n" +
+                        "Comme plat principal: Blanquette de veau, Lasagnes à la bolognaise, Courge spaghetti à la crème et aux lardons.\n" +
+                        "Comme dessert: Mousse au Chocolate et au Raspberry , Brownies au Chocolat, Cheesecake.\n");
+        questionAnswerFr.put("price-inquiry", "entrée: 20$ -- 25$\n" +
+                "plat principal: 25$ -- 70$\n" +
+                "dessert: 30$ -- 50$\n");
+        questionAnswerFr.put("conversation-continue", "Comment je peux vous aider d'avantage?");
+        questionAnswerFr.put("conversation-complete", "Ravi de communiquer avec vous.");
+
+    }
+}
--- Backend/src/main/java/com/example/RestaurantChat/services/ProcessingMsgsService.java
+++ Backend/src/main/java/com/example/RestaurantChat/services/ProcessingMsgsService.java
@@ -27,43 +27,12 @@ public class ProcessingMsgsService {
     private static Map<String, String> questionAnswer = new HashMap<>();
     private static Map<String, String> questionAnswerFr = new HashMap<>();
 
-    /*
-     * Define answers for each given category.
-     */
-    static {
-        questionAnswer.put("greeting", "Hello, how can I help you?");
-        questionAnswer.put("menu-inquiry",
-                "for the menu whe have:\n" +
-                        "as appetizers: Guacamole, Balsamic Bruschetta, Hot Spinach Artichoke Dip.\n" +
-                        "as main dishes: Cedar-Plank Salmon, Seared Scallops With Brown Butter and Lemon Pan Sauce.\n" +
-                        "as dessert: Chocolate Raspberry Mousse, BrowniesChocolate, Cheesecake.\n");
-        questionAnswer.put("price-inquiry", "appetizers: 20$ -- 25$\n" +
-                "main dish: 25$ -- 70$\n" +
-                "dessert: 30$ -- 50$\n");
-        questionAnswer.put("conversation-continue", "What else can I help you with?");
-        questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
-
-    }
-
-    static {
-        questionAnswerFr.put("greeting", "Hey, Comment je peux vous aider?");
-        questionAnswerFr.put("menu-inquiry",
-                "Pour le menu on a:\n" +
-                        "Comme entrée: Guacamole, Velouté potimarron et lait d'amande, Oeufs mollets champignons bacon.\n" +
-                        "Comme plat principal: Blanquette de veau, Lasagnes à la bolognaise, Courge spaghetti à la crème et aux lardons.\n" +
-                        "Comme dessert: Mousse au Chocolate et au Raspberry , Brownies au Chocolat, Cheesecake.\n");
-        questionAnswerFr.put("price-inquiry", "entrée: 20$ -- 25$\n" +
-                "plat principal: 25$ -- 70$\n" +
-                "dessert: 30$ -- 50$\n");
-        questionAnswerFr.put("conversation-continue", "Comment je peux vous aider d'avantage?");
-        questionAnswerFr.put("conversation-complete", "Ravi de communiquer avec vous.");
-
-    }
-
-
-
-
+    private InitializeEnglishAnswers initializeEnglishAnswers = new InitializeEnglishAnswers();
+    
     public Message processMsg(Message message) throws IOException, InterruptedException {
+
+        initializeEnglishAnswers.initializeAnswers(questionAnswer);
+        initializeEnglishAnswers.initializeFrAnswers(questionAnswerFr);
 
         //Prepare the response: Message Object
         Message chatBotResponse = new Message();
